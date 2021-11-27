f33a5e665532713b96350caeeb6d1692a69f2ac0
Iliya <91027691+Melekit235@users.noreply.github.com>
2021-11-28 02:52:41 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# test69
- 
+# Snake
+Educational project
---
+++ src/test/java/MenuTest.java
@@ -0,0 +1,15 @@
+/*import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTest {
+
+    @Test
+    @DisplayName("Big field")
+    public void big_field(){
+
+
+}
+
+}*/
